c26527974af41ba46f13c5283bec51f95233073c
AbhishekGag <137916055+AbhishekGag@users.noreply.github.com>
2023-06-29 23:13:24 +0530

    Delete sentiment_app_run.sh

--- sentiment_app_run.sh
+++
@@ -1,9 +0,0 @@
-APP_NAME="sentiment-analysis-nlp-bert"
-CONTAINER_IMAGE_URL="us.icr.io/sn-labsassets/sentiment-bert-watson-nlp-runtime"
-VISIBILITY=project
-ibmcloud ce application create \
---name ${APP_NAME} \
---env ACCEPT_LICENSE=true \
---image ${CONTAINER_IMAGE_URL} \
---registry-secret icr-secret \
---visibility ${VISIBILITY}
