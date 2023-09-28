689825324edbb02ea0301f158f9ad09e729f60a0
JackSavino <jack_savino@yahoo.com>
2023-09-28 19:53:49 -0400

    Adding updated sentiment_analysis.py file

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,11 +1,18 @@
-import requests
+'''
+Module for Sentiment Analysis package
+'''
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    '''
+    Used to analyze the text input by the user and return the appropriate message
+    '''
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson\
+    .runtime.nlp.v1/NlpService/SentimentPredict'
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj = {"raw_document": {"text": text_to_analyse}}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers = header, timeout = 5)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
