9d071c613440fcf2ed436cd5a2256c6972fe1d67
Aleksandr <alexk.vendors@gmail.com>
2024-02-18 19:07:30 +0100

    import and app initiation

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
 app = Flask("Sentiment Analyzer")
 ''' Executing this function initiates the application of sentiment
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
 
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
 
-#Initiate the flask app : TODO
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
