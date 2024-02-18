254636610bb739ea46d00dacca9e00d87de61a22
Aleksandr <alexk.vendors@gmail.com>
2024-02-18 20:21:20 +0100

    task_6-complete

--- server.py
+++ server.py
@@ -11,21 +11,25 @@ app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    
+    text_to_analyze = request.arg.get("textToAnalyze")
+    result = sentiment_analyzer(text_to_analyze)
+    return result
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    
+    
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template("index.html") 
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000, debug=True)
